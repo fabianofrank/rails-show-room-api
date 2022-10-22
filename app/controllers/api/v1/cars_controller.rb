20d052dc234c085b4b92a0e82d712c7cb4b527d2
fabianofrank <fabianofrank@protonmail.com>
2022-10-22 17:23:30 -0300

    Modify image

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -4,21 +4,21 @@ class Api::V1::CarsController < ApplicationController
   # GET /cars
   def index
     # @cars = Car.all.to_json(include: [:image])
-    @cars = []
-    Car.all.each do |car|
-      @cars.push(car.as_json.merge({ image: url_for(car.image) }))
-    end
+    @cars = Car.all
+    # Car.all.each do |car|
+    #   @cars.push(car.as_json.merge({ image: url_for(car.image) }))
+    # end
 
     render json: @cars
   end
 
   # GET /cars/1
   def show
-    @res = []
+    # @res = []
     @car = Car.find(params[:id])
-    @res.push(@car.as_json.merge({ image: url_for(@car.image) }))
+    # @res.push(@car.as_json.merge({ image: url_for(@car.image) }))
 
-    render json: @res
+    render json: @car
   end
 
   # POST /cars
